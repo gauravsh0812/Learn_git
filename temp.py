109929574b36a6d03d16865d2deaf25168d81f9f
Gaurav Sharma <57118584+gauravsh0812@users.noreply.github.com>
2021-01-11 22:07:19 -0500

    Update temp.py

--- temp.py
+++ temp.py
@@ -36,7 +36,7 @@ Unicode_logger = logging.getLogger()
 
 def main():
 
-    for yr in [14, 15, 16, 17, 18]:
+    for yr in [15, 16, 17, 18]:
         for month in ['01', '02','03','04','05','06','07','08','09','10','11','12']:
 
             source_path = '/projects/temporary/automates/er/gaurav'
