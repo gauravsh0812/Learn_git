23ffc6c8d69f173b4d219541ab59634ab8d378ee
gauravsh0812 <gauravs@email.arizona.edu>
2021-01-25 18:31:48 -0500

    modifying token_wise

--- token_wise_distribution.py
+++ token_wise_distribution.py
@@ -1,47 +1,92 @@
 # Parsing all the working tokens
 import json
-import os
+import os, subprocess
 import matplotlib.pyplot as plt
+import multiprocessing
+import argparse
+
+from datetime import datetime
+from multiprocessing import Pool, Lock
+
 
 # counters
-zero_token = 0
-one2three_tokens = 0
-four2five_tokens = 0
-six2ten_tokens = 0
-ten_or_more = 0
+#zero_token = 0
+#one2three_tokens = 0
+#four2five_tokens = 0
+#six2ten_tokens = 0
+#ten_or_more = 0
+
+alphabet_Set = list(string.ascii_lowercase)+list(string.ascii_uppercase)
 
 def main():
 
-    for yr in [14, 15, 16, 17, 18]:
+    for yr in [14]:#, 15, 16, 17, 18]:
         year = '20' + str(yr)
+        subprocess.call(['mkdir', f'/projects/temporary/automates/er/gaurav/{year}/eqn_token_distribution'])
 
-        for m in ['01','02','03','04','05','06','07','08','09','10','11','12']:
+        for m in ['01'];#,'02','03','04','05','06','07','08','09','10','11','12']:
             month = str(yr)+m
+
+            subprocess.call(['mkdir', f'/projects/temporary/automates/er/gaurav/{year}/eqn_token_distribution/{month}'])
 
             latex_eqn_path = f'/projects/temporary/automates/er/gaurav/{year}/{month}/latex_equations'
             etree_path = f'/projects/temporary/automates/er/gaurav/{year}/{month}/etree'
 
+            temp_args = []
+
             for folder in os.listdir(etree_path):
+                subprocess.call(['mkdir', f'/projects/temporary/automates/er/gaurav/{year}/eqn_token_distribution/{month}/{folder}'])
                 for tyf in ['Small_MML', 'Large_MML']:
                     tyf_path = os.path.join(etree_path, f'{folder}/{tyf}')
 
                     for text_file in os.listdir(tyf_path):
-                        # open the same file in latex_equations
-                        tyf_le = 'Small_eqns' if tyf == 'Small_MML' else 'Large_eqn'
-                        file_name = text_file.split('.')[0]
-                        original_eqn_path = os.path.join(latex_eqn_path, f'{folder}/{tyf_le}/{file_name}.txt')
-                        eqn = open(original_eqn_path, 'r').readlines()[0]
-                        count_token(eqn)
+                        temp.append([year, month, folder, tyf, text_file, latex_eqn_path])
+
+            with Pool(multiprocessing.cpu_count()-10) as pool:
+                result = pool.map(count_token, temp)
 
     # creating bins -- distribution
-    bin_plot()
+    #bin_plot()
 
-def count_token(eqn):
+def count_token(args_list):
+
+    # unpacking args_list
+    (year, month, folder, tyf, text_file, latex_eqn_path) = args_list
+
+    tyf_le = 'Small_eqns' if tyf == 'Small_MML' else 'Large_eqn'
+    subprocess.call(['mkdir', f'/projects/temporary/automates/er/gaurav/{year}/eqn_token_distribution/{month}/{folder}/{tyf_le}'])
+
+    file_name = text_file.split('.')[0]
+    original_eqn_path = os.path.join(latex_eqn_path, f'{folder}/{tyf_le}/{file_name}.txt')
+    equation = open(original_eqn_path, 'r').readlines()[0]
 
     # count total number of tokens
-    tokens_index = [i for i, char in enumerate(eqn) if char == '\\']
-    number_of_tokens = len(tokens_index)
+    char_list=[char for char in equation]
+    temp_token_list = []
+    i, begin, end, alpha = 0,0,0, False
 
+    while i <len(char_list):
+
+        if char_list[i] in "\\":
+            begin =i
+            alpha=True
+
+        if alpha==True:
+            if i!=len(char_list)-1:
+                if char_list[i+1] not in alphabet_Set:
+                    alpha=False
+                    end=i
+                    token = equation[begin:end+1]
+                    #print(token)
+                    if token not in temp_token_list:
+                        temp_token_list.append(token)
+        i+=1
+
+    number_of_tokens = len(temp_token_list)
+
+    json.dump(number_of_tokens, open(f'/projects/temporary/automates/er/gaurav/{year}/eqn_token_distribution/{month}/{folder}/{tyf_le}/{file_name}.json', 'w'))
+
+    '''
     if number_of_tokens == 0:
         zero_token+=1
     elif 1 <= number_of_tokens <=3:
@@ -51,7 +96,8 @@ def count_token(eqn):
     elif 6<= number_of_tokens <=10:
         six2ten_tokens+=1
     else: ten_or_more+=1
-
+    '''
+'''
 def bin_plot():
 
     temp = {}
@@ -65,7 +111,7 @@ def bin_plot():
     plt.figure(figsize=(15,5))
     plt.bar(temp.keys(), temp.values(), width, color='g')
     plt.savefig('/projects/temporary/automates/er/gaurav/token_equation_distribution.png')
-
+'''
 
 if __name__ == '__main__':
     main()
