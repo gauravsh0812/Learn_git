a99f37467a5704753672e2c549c9784411322c6c
Gaurav Sharma <57118584+gauravsh0812@users.noreply.github.com>
2021-05-18 18:21:10 -0400

    Update mml_tokens.py

--- mml_tokens.py
+++ mml_tokens.py
@@ -1,44 +1,36 @@
 # -*- coding: utf-8 -*-
 import os, glob, json
 
-MML = '/projects/temporary/automates/er/gaurav/2014/1401/Simplified_mml/*'
+def main():
 
-tokens=[]
-F = open('/projects/temporary/automates/er/gaurav/MML_tokens.txt', 'w') 
+    MML = '/projects/temporary/automates/er/gaurav/2014/1401/Simplified_mml/*'
 
-for folder in glob.glob(MML):
-    Path1 = os.path.join(MML, folder)
-    print('------------'*5)
-    print(os.path.basename(Path1))
-    for tyf in glob.glob(os.path.join(Path1, '*')):
-        Path2=os.path.join(Path1, tyf)
-        for file in glob.glob(os.path.join(Path2, '*')):
-            eqn = open(file, 'r').readlines()[0]
+    tokens=[]
+    F = open('/projects/temporary/automates/er/gaurav/MML_tokens.txt', 'w') 
 
-            open_angle = [idx_open for idx_open, angle in enumerate(eqn) if angle == '<']
-            close_angle = [idx_close for idx_close, angle in enumerate(eqn) if angle == '>']
-            #print('---------------'*5)
-            #print(eqn)
-            for i in range(len(open_angle)):
-                #print(i)
-                token1 = eqn[open_angle[i]:close_angle[i]+1]
-                #print('token1: ', token1)
-                if token1 not in tokens:
-                    F.write(f'{token1}\n')
-                    #tokens.append(token1)
-                    #print(token1)
-                    if i<len(open_angle)-1:
-                        token2 = eqn[close_angle[i]+1:open_angle[i+1]]
-                        #print('token2: ', token2)
-                        token2=token2.strip()
-                        #if token2 != " ":
-                        if token2 not in tokens:
-                            F.write(f'{token2}\n')
-                            #print(token2)
-                            #tokens.append(token2)
+    for folder in glob.glob(MML):
+        Path1 = os.path.join(MML, folder)
+        print('------------'*5)
+        print(os.path.basename(Path1))
+        for tyf in glob.glob(os.path.join(Path1, '*')):
+            Path2=os.path.join(Path1, tyf)
+            for file in glob.glob(os.path.join(Path2, '*')):
+                eqn = open(file, 'r').readlines()[0]
 
-#with open('/projects/temporary/automates/er/gaurav/MML_tokens.json', 'w') as F:
-#    json.dump(tokens, F)
+                open_angle = [idx_open for idx_open, angle in enumerate(eqn) if angle == '<']
+                close_angle = [idx_close for idx_close, angle in enumerate(eqn) if angle == '>']
 
-print('Job Done!')
+                for i in range(len(open_angle)):
+                    token1 = eqn[open_angle[i]:close_angle[i]+1]
+                    if token1 not in tokens:
+                        F.write(f'{token1}\n')
+                        if i<len(open_angle)-1:
+                            token2 = eqn[close_angle[i]+1:open_angle[i+1]]
+                            token2=token2.strip()
+                            if token2 not in tokens:
+                                F.write(f'{token2}\n')
+                
 
+if __name__=='__main()':
+    main()
+    print('Job Done!')
