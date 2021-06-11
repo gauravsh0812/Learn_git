ce68dd08d8a31fcdfa3779f29c62d729245d89fe
Gaurav Sharma <gauravs@sista.arizona.edu>
2021-06-11 15:16:04 -0700

    add

---
+++ jsfile.js
@@ -0,0 +1,47 @@
+eqn_src = [
+  {
+    eqn_num: 1,
+    tgt: '<math>  <msub>  <mi> I </mi>  <mrow>  <mn> 11 </mn>  </mrow>  </msub>  <mo> ( </mo>  <mi> r </mi>  <mo> ) </mo>  <mo> = </mo>  <mtext> &#xA0; </mtext>  <msqrt>   <mstyle>   <mrow>  <mfrac>  <mn> 2 </mn>  <mrow>  <msup>  <mi> l </mi>  <mn> 2 </mn>  </msup>  <mo> &#x2212; <!-- â\\x88\\x92 -->  </mo>  <mi> m </mi>  </mrow>  </mfrac>  </mrow>  </mstyle>  </msqrt>  <mi> arctan </mi>  <mo> &#x2061; <!-- â\\x81¡ -->  </mo>  <mrow>  <mo> ( </mo>  <msqrt>   <mstyle>   <mrow>  <mfrac>  <mn> 2 </mn>  <mrow>  <msup>  <mi> l </mi>  <mn> 2 </mn>  </msup>  <mo> &#x2212; <!-- â\\x88\\x92 -->  </mo>  <mi> m </mi>  </mrow>  </mfrac>  </mrow>  </mstyle>  </msqrt>  <mtext> &#xA0; </mtext>  <mi> r </mi>  <mo> ) </mo>  </mrow>   <mstyle>   </math> ',
+    pred: '<math> <msub> <mi> I </mi> <mrow> <mn> 11 </mn> </mrow> </msub> <mo> ( </mo> <mi> r </mi> <mo> ) </mo> <mo> = </mo> <mrow>'
+  },
+  {
+    eqn_num: 2,
+    tgt: '<math>  <msub>  <mrow>  <mi> H </mi>  </mrow>  <mrow> ',
+    pred: '<math> <mrow> <mi> H </mi> </mrow> <mo> &#x223C; <!-- â\\x88¼ --> </mo> <mstyle> <mrow> <mfrac> <mrow> <mn> 4 </mn> <msub> <mi> G </mi> <mi> F </mi> </msub> </mrow> <mrow> <mn> 2 </mn> <msqrt> <mn> 2 </mn> </msqrt> </mrow> </mfrac> </mrow> <mrow> <mi> c </mi> <mi> o </mi> <mi> s </mi> </mrow> <msub> <mi> &#x03D1; <!-- Ï\\x91 --> </mi> <mi> C </mi> </msub> <mrow> <mi> s </mi> <mi> i </mi> <mi> n </mi> </mrow> <msub> <mi> &#x03D1; <!-- Ï\\x91 --> </mi> <mi> C </mi> </msub> <munderover> <mo> &#x2211; <!-- â\\x88\\x91 --> </mo> <mrow> <mi> i </mi> <mo> = </mo> <mn> 1 </mn> </mrow> <mn> 6 </mn> </munderover> <msub> <mi> c </mi> <mi> i </mi> </msub> <msub> <mrow> <mi> O </mi> </mrow> <mi> i </mi> </msub> </mstyle> </math>'
+  },
+  {
+    eqn_num: 9,
+    tgt: '<math>  <mi> S </mi>  <mi> M </mi>  <mo> = </mo>  <mstyle scriptlevel="0">  <mrow>   <mi>  { </mo>  </mrow>  </mstyle>  <mi> &#x03C8; <!-- Ï\\x88 -->  </mi>  <mo> : </mo>  <mo> [ </mo>  <mn> 0 </mn>  <mo> , </mo>  <mi> T </mi>  <mo> ] </mo>  <mo> &#x00D7; <!-- Ã\\x97 -->  </mo>  <msup>  <mrow>   <mi>  T </mi>  </mrow>  <mn> 2 </mn>  </msup>  <mo> &#x00D7; <!-- Ã\\x97 -->  </mo>  <mi> &#x03A9; <!-- Î© -->  </mi>  <mo> &#x2192; <!-- â\\x86\\x92 -->  </mo>  <msup>  <mrow>   <mi>  T </mi>  </mrow>  <mn> 2 </mn>  </msup>  <mo> : </mo>  <mi> &#x03C8; <!-- Ï\\x88 -->  </mi>   <mstyle>   <mtext> &#xA0;measurable&#xA0; </mtext>  </mstyle>  <mo> , </mo>   <mstyle>   <munder>   <mi>  sup </mo>  <mrow>  <mo> [ </mo>  <mn> 0 </mn>  <mo> , </mo>  <mi> T </mi>  <mo> ] </mo>  </mrow>  </munder>  <msub>  <mo> &#x222B; <!-- â\\x88« -->  </mo>  <mrow>  <msup>  <mrow>   <mi>  T </mi>  </mrow>  <mn> 2 </mn>  </msup>  </mrow>  </msub>  <mi> E </mi>  <mo> [ </mo>  <mrow>  <mo> | </mo>  </mrow>  <msub>  <mi> &#x03C8; <!-- Ï\\x88 -->  </mi>  <mi> t </mi>  </msub>  <mo> ( </mo>  <mi> x </mi>  <mo> ) </mo>  <mrow>  <mo> | </mo>  </mrow>  <mo> ] </mo>  <mi> d </mi>  <mi> x </mi>  <mo> &lt; </mo>  <mo> + </mo>  <mi> &#x221E; <!-- â\\x88\\x9e -->  </mi>  <mo> , </mo>  <msub>  <mi> &#x03C8; <!-- Ï\\x88 -->  </mi>  <mi> t </mi>  </msub>   <mstyle>   <mtext> &#xA0;meas.-pres. for a.e.&#xA0; </mtext>  </mstyle>  <mo> ( </mo>  <mi> t </mi>  <mo> , </mo>  <mi> &#x03C9; <!-- Ï\\x89 -->  </mi>  <mo> ) </mo>  <mstyle scriptlevel="0">  <mrow>   <mi>  } </mo>  </mrow>  </mstyle>  </math> ',
+    pred: '<math> <msub> <mi> S </mi> <mrow>'
+  },
+  {
+    eqn_num: 10,
+    tgt: '<math>   <mstyle>    <mstyle>    <mstyle>    <mstyle>    <mstyle>    <mstyle>    <mstyle>    <mstyle>    <mstyle>    <mstyle>    <mstyle>    <mstyle>    <mstyle>    <mstyle>    <mstyle>    <mstyle>   </math> ',
+    pred: '<math> <mstyle> </math>'
+  },
+  {
+    eqn_num: 11,
+    tgt: '<math>   <mstyle>   </math> ',
+    pred: '<math> <mstyle> </math>'
+  },
+  {
+    eqn_num: 12,
+    tgt: '<math>  <msub>  <mi> &#x03A6; <!-- Î¦ -->  </mi>  <mrow>  <mi> i </mi>  <mo> , </mo>  <mn> 12 </mn>  </mrow>  </msub>  <msubsup>  <mi> &#x03A6; <!-- Î¦ -->  </mi>  <mrow>  <mi> i </mi>  <mo> , </mo>  <mn> 11 </mn>  </mrow>  <mrow>  <mi> T </mi>  </mrow>  </msubsup>   <mo>    <mo>    <mo>    <mo>   <mo> = </mo>   <mo>    <mo>    <mo>    <mo>   <mrow>  <mo> [ </mo>  <msup>  <mi> G </mi>  <mrow>  <mo> [ </mo>  <mn> 1 </mn>  <mo> ] </mo>  </mrow>  </msup>  <msup>  <mi> G </mi>  <mrow>  <mo> [ </mo>  <mn> 1 </mn>  <mo> ] </mo>  <mi> T </mi>  </mrow>  </msup>  <mo> ( </mo>  <msup>  <mi> A </mi>  <mrow>  <mo> [ </mo>  <mn> 1 </mn>  <mo> ] </mo>  </mrow>  </msup>  <msup>  <mo> ) </mo>  <mrow>  <mo> &#x2212; <!-- â\\x88\\x92 -->  </mo>  <mi> T </mi>  </mrow>  </msup>  <mo> ] </mo>  </mrow>  <msup>  <mi> A </mi>  <mrow>  <mo> [ </mo>  <mn> 1 </mn>  <mo> ] </mo>  <mi> T </mi>  </mrow>  </msup>  <mo> = </mo>  <msup>  <mi> G </mi>  <mrow>  <mo> [ </mo>  <mn> 1 </mn>  <mo> ] </mo>  </mrow>  </msup>  <msup>  <mi> G </mi>  <mrow>  <mo> [ </mo>  <mn> 1 </mn>  <mo> ] </mo>  <mi> T </mi>  </mrow>  </msup>  </math> ',
+    pred: '<math> <msubsup> <mi> &#x03A6; <!-- Î¦ --> </mi> <mrow> <mi> i </mi> <mo> , </mo> <mn> 12 </mn> </mrow> <mrow>'
+  },
+  {
+    eqn_num: 13,
+    tgt: '<math>  <msup>  <mrow> ',
+    pred: '<math> <msup> <mrow>'
+  },
+  {
+    eqn_num: 14,
+    tgt: '<math>  <msub>  <mi> B </mi>  <mi> &#x039B; <!-- Î\\x9b -->  </mi>  </msub>  <mo> = </mo>  <mi> E </mi>  <msup>  <mo> ( </mo>  <mrow>  <mi> A </mi>  </mrow>  </msup>  <mrow>  <mi> Z </mi>  </mrow>  <mo> ( </mo>  <msup>  <mi> j </mi>  <mi> &#x03C0; <!-- Ï\\x80 -->  </mi>  </msup>  <mo> ) </mo>  <mo> ) </mo>  <mo> &#x2212; <!-- â\\x88\\x92 -->  </mo>  <mi> E </mi>  <msubsup>  <mo> ( </mo>  <mi> &#x039B; <!-- Î\\x9b -->  </mi>  <mrow>  <mi> A </mi>  <mo> + </mo>  <mn> 1 </mn>  </mrow>  </msubsup>  <mrow>  <mi> Z </mi>  </mrow>  <mo> ( </mo>  <msup>  <mi> J </mi>  <mi> &#x03C0; <!-- Ï\\x80 -->  </mi>  </msup>  <mo> ) </mo>  <mo> ) </mo>  </math> ',
+    pred: '<math> <msub> <mi> B </mi> <mi> &#x039B; <!-- Î\\x9b --> </mi> </msub> <mo> = </mo> <mi> E </mi> <mo> ( </mo> <msup> <mrow>'
+  },
+  {
+    eqn_num: 15,
+    tgt: '<math>  <mtext> &#xA0; </mtext>  </math> ',
+    pred: '<math> <mstyle> </math>'
+  },
+];
