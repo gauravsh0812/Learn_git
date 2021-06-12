3a6b66b0647c365a62afbfdbe0cb81ac03230ab0
Gaurav Sharma <gauravs@sista.arizona.edu>
2021-06-11 17:50:12 -0700

    add

--- temp.py
+++ temp.py
@@ -1,44 +1,8 @@
-import xml.etree.ElementTree as ET
-import subprocess, os, glob, multiprocessing
-import sys
-import xml.dom.minidom
-from xml.etree.ElementTree import ElementTree
-from multiprocessing import Pool, Lock
+pred = open('/home/gauravs/Automates/temp/Learn_git/pred.txt', 'r+').readlines()
+tgt = open('/home/gauravs/Automates/temp/Learn_git/tgt-test.txt', 'r+').readlines()
 
-lock = Lock()
-
-mp = '/projects/temporary/automates/er/gaurav/rough/p/*'
-mt = '/projects/temporary/automates/er/gaurav/rough/t/*'
-
-temp = []
-for m in [mp,mt]:
-    for f in glob.glob(m):
-        temp.append([f, m])
-with Pool(multiprocessing.cpu_count()-10) as pool:
-            result = pool.map(pooling, temp)
-
-def pooling(args_list):
-    f, m = args_list
-    # converting text file to xml formatted file
-    basename = os.path.basename(f).split('.')[0]
-    tree1 = ElementTree()
-    tree1.parse(f)
-    if m==mp:
-        typ_m = 'p'
-        typ_sm = 'sp'
-    else: 
-        typ_m = 't'
-        typ_sm = 'st'
-    sample_path = '/projects/temporary/automates/er/gaurav/rough/sample/{typ_sm}/{basename}.xml'
-    tree1.write(sample_path)
-
-    # Writing etree for the xml files
-    tree = ET.parse(sample_path)
-    xml_data = tree.getroot()
-    xmlstr = ET.tostring(xml_data, encoding='utf-8', method='xml')
-    input_string=xml.dom.minidom.parseString(xmlstr)
-    xmlstr = input_string.toprettyxml()
-    xmlstr= os.linesep.join([s for s in xmlstr.splitlines() if s.strip()])
-
-    with open('/projects/temporary/automates/er/gaurav/rough/sample/{typ_m}/{basename}.xml', "wb") as file_out:
-        file_out.write(xmlstr.encode(sys.stdout.encoding, errors='replace'))
+for i, e in enumerate(pred):
+    if '<math> <mstyle> </math>\n\n' in pred:
+        pred = pred.remove(i) 
+        tgt = tgt.remove(i)
+    
