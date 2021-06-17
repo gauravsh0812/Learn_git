daf2820e8d63ba63514e2be4c1bbd91440537207
Gaurav Sharma <gauravs@sista.arizona.edu>
2021-06-17 12:33:19 -0700

    add

--- JS/t.py
+++ JS/t.py
@@ -7,11 +7,12 @@ def json2js(json_data, output_file, var_name='eqn_src'):
         fout.write(f'{var_name} = [\n')
         for i, datum in enumerate(json_data):
             #print(datum)
-            fout.write('  {\n')
-            fout.write(f'    eqn_num: {repr(datum["eqn_num"])},\n')
-            fout.write(f'    src: {repr(datum["src"])},\n')
-            fout.write(f'    mml: {repr(datum["mml"])}\n')
-            fout.write('  }')
+            if len(datum)>5:
+                fout.write('  {\n')
+                fout.write(f'    eqn_num: {repr(datum["eqn_num"])},\n')
+                fout.write(f'    src: {repr(datum["src"])},\n')
+                fout.write(f'    mml: {repr(datum["mml"])}\n')
+                fout.write('  }')
             if i < len(json_data):
                 fout.write(',')
             fout.write('\n')
@@ -24,9 +25,9 @@ if __name__ == '__main__':
     while i<100:
         f = open(f'score-{i}-{i+5}.txt', 'r').readlines()
         json_data = []
-        temp_dict={}
+        #temp_dict={}
         for idx, v in enumerate(f):
-            #temp_dict={}
+            temp_dict={}
             if 'tgt' in v:
                 temp_dict['eqn_num'] = idx
                 temp_dict['src'] = v[5:]
@@ -34,7 +35,7 @@ if __name__ == '__main__':
                 temp_dict['mml'] = v[6:]
 
             json_data.append(temp_dict)
-        #print(json_data)
+        #print(json_data[0:5])
         i+=5
         json2js(json_data, f'JS/score-{i}-{i+5}.js')
 
