e3eefd0fa4f08979b5b536862ddab5c32f020e33
Gaurav Sharma <gauravs@sista.arizona.edu>
2021-06-19 11:16:18 -0700

    add

---
+++ score-80-85.js
@@ -0,0 +1,102 @@
+eqn_src = [
+  {
+    eqn_num: 1482,
+    mml1: '<math>  <mi> &#x0394; <!-- Î\x94 -->  </mi>  <msub>  <mi> t </mi>  <mn> 1 </mn>  </msub>  <mo> = </mo>  <mn> 8 </mn>  <mtext> &#xA0; </mtext>  <mrow>  <mi> h </mi>  </mrow>  </math> \n',
+    mml2: '<math> <mi> &#x0394; <!-- Î\x94 --> </mi> <msub> <mi> t </mi> <mn> 1 </mn> </msub> <mo> = </mo> <mn> 8 </mn> <mtext> &#xA0; </mtext> <mi> h </mi> </math>\n'
+  },
+  {
+    eqn_num: 3317,
+    mml1: '<math>  <mi> Q </mi>  <mo> = </mo>  <mn> 8 </mn>   <mo>   <mrow>  <mi> G </mi>  <mi> e </mi>  <mi> V </mi>  </mrow>  </math> \n',
+    mml2: '<math> <mi> Q </mi> <mo> = </mo> <mn> 8 </mn> <mstyle> <mrow> <mi> G </mi> <mi> e </mi> <mi> V </mi> </mrow> </math>\n'
+  },
+  {
+    eqn_num: 780,
+    mml1: '<math>  <mi> f </mi>  <mrow>  <mi> i </mi>  <mi> n </mi>  </mrow>  <mrow>  <mi> d </mi>  <mi> o </mi>  <mi> m </mi>  </mrow>  <mo> ( </mo>  <msup>  <mrow>  <mi> H </mi>  </mrow>  <mn> 0 </mn>  </msup>  <mo> ) </mo>  </math> \n',
+    mml2: '<math> <mi> f </mi> <mrow> <mi> i </mi> <mi> n </mi> </mrow> <mrow> <mi> d </mi> <mi> o </mi> <mi> m </mi> </mrow> <mo> ( </mo> <msup> <mi> H </mi> <mn> 0 </mn> </msup> <mo> ) </mo> </math>\n'
+  },
+  {
+    eqn_num: 2112,
+    mml1: '<math>  <msub>  <mi> &#x03BB; <!-- Î» -->  </mi>  <mn> 3 </mn>  </msub>  <mo> ( </mo>  <mi> G </mi>  <mo> &#x2218; <!-- â\x88\x98 -->  </mo>  <mi> H </mi>  <mo> ) </mo>  <mo> &#x2A7D; <!-- â©½ -->  </mo>  <mrow>  <mrow>  <mi> m </mi>  <mi> i </mi>  <mi> n </mi>  </mrow>  </mrow>  <mstyle scriptlevel="0">  <mrow>   <mi>  { </mo>  </mrow>  </mstyle>  <mstyle scriptlevel="0">  <mrow>   <mi>  &#x230A; </mo>  </mrow>  </mstyle>  </math> \n',
+    mml2: '<math> <msub> <mi> &#x03BB; <!-- Î» --> </mi> <mn> 3 </mn> </msub> <mo> ( </mo> <mi> G </mi> <mo> &#x2218; <!-- â\x88\x98 --> </mo> <mi> H </mi> <mo> ) </mo> <mo> &#x2A7D; <!-- â©½ --> </mo> <mi> min </mo> <mstyle scriptlevel="0"> <mrow> <mi> { </mo> </mrow> </mstyle> <mstyle scriptlevel="0"> <mrow> <mi> | </mo> </mrow> </mstyle> </math>\n'
+  },
+  {
+    eqn_num: 2233,
+    mml1: '<math>   <mo>  &#x27E8; <!-- â\x9f¨ -->  </mo>  <msup>  <mrow>  <mo> ( </mo>  <msubsup>  <mi> S </mi>  <mrow>  <mrow>  <mi> L </mi>  </mrow>  </mrow>  <mrow>  <mi> z </mi>  </mrow>  </msubsup>  <mo> ) </mo>  </mrow>  <mn> 2 </mn>  </msup>   <mo>  &#x27E9; <!-- â\x9f© -->  </mo>  <mo> = </mo>  <mo> &#x2212; <!-- â\x88\x92 -->  </mo>   <mo>  &#x27E8; <!-- â\x9f¨ -->  </mo>  <msubsup>  <mi> S </mi>  <mrow>  <mrow>  <mi> L </mi>  </mrow>  </mrow>  <mrow>  <mi> z </mi>  </mrow>  </msubsup>  <msubsup>  <mi> S </mi>  <mrow>  <mrow>  <mi> R </mi>  </mrow>  </mrow>  <mrow>  <mi> z </mi>  </mrow>  </msubsup>   <mo>  &#x27E9; <!-- â\x9f© -->  </mo>  </math> \n',
+    mml2: '<math> <mo> &#x27E8; <!-- â\x9f¨ --> </mo> <mo> ( </mo> <msubsup> <mi> S </mi> <mrow> <mrow> <mi> L </mi> </mrow> </mrow> <mrow> <mi> z </mi> </mrow> </msubsup> <msup> <mo> ) </mo> <mrow> <mn> 2 </mn> </mrow> </msup> <mo> &#x27E9; <!-- â\x9f© --> </mo> <mo> = </mo> <mo> &#x2212; <!-- â\x88\x92 --> </mo> <mo> &#x27E8; <!-- â\x9f¨ --> </mo> <msubsup> <mi> S </mi> <mrow> <mrow> <mi> L </mi> </mrow> </mrow> <mrow> <mi> z </mi> </mrow> </msubsup> <msubsup> <mi> S </mi> <mrow> <mrow> <mi> R </mi> </mrow> </mrow> <mrow> <mi> z </mi> </mrow> </msubsup> <mo> &#x27E9; <!-- â\x9f© --> </mo> </math>\n'
+  },
+  {
+    eqn_num: 1861,
+    mml1: '<math>  <msub>  <mi> P </mi>  <mi> i </mi>  </msub>  <mo> = </mo>  <msub>  <mi> L </mi>  <mi> i </mi>  </msub>  <mo> + </mo>  <msub>  <mi> D </mi>  <mrow>  <mi> i </mi>  </mrow>  </msub>  <msub>  <mi> Q </mi>  <mi> i </mi>  </msub>  </math> \n',
+    mml2: '<math> <msub> <mi> P </mi> <mi> i </mi> </msub> <mo> = </mo> <msub> <mi> L </mi> <mi> i </mi> </msub> <mo> + </mo> <msub> <mi> D </mi> <mi> i </mi> </msub> <msub> <mi> Q </mi> <mi> i </mi> </msub> </math>\n'
+  },
+  {
+    eqn_num: 588,
+    mml1: '<math>  <mi> L </mi>  <mo> ( </mo>  <msub>  <mi> f </mi>  <mi> i </mi>  </msub>  <mo> ) </mo>  <mo> = </mo>  <msub>  <mi> D </mi>  <mrow>  <mi> i </mi>  </mrow>  </msub>  <mo> ( </mo>  <mi> g </mi>  <mo> ) </mo>  </math> \n',
+    mml2: '<math> <mi> L </mi> <mo> ( </mo> <msub> <mi> f </mi> <mi> i </mi> </msub> <mo> ) </mo> <mo> = </mo> <msub> <mi> D </mi> <mi> i </mi> </msub> <mo> ( </mo> <mi> g </mi> <mo> ) </mo> </math>\n'
+  },
+  {
+    eqn_num: 329,
+    mml1: '<math>  <mrow>  <mfrac>  <msub>  <mi> R </mi>  <mrow>  <mi> s </mi>  <mi> g </mi>  </mrow>  </msub>  <msub>  <mi> R </mi>  <mrow>  <mi> B </mi>  <mi> H </mi>  </mrow>  </msub>  </mfrac>  </mrow>  <mo> &#x2243; <!-- â\x89\x83 -->  </mo>  <mn> 5 </mn>  <mo> &#x00D7; <!-- Ã\x97 -->  </mo>  <msup>  <mn> 10 </mn>  <mn> 2 </mn>  </msup>  <msubsup>  <mi> &#x03B1; <!-- Î± -->  </mi>  <mrow>  <mi> t </mi>  <mo> , </mo>  <mn> 01 </mn>  </mrow>  <mrow>  <mn> 28 </mn>  <mrow>  <mo> / </mo>  </mrow>  <mn> 45 </mn>  </mrow>  </msubsup>  <msubsup>  <mi> M </mi>  <mrow>  <mi> B </mi>  <mi> H </mi>  <mo> , </mo>  <mn> 8 </mn>  </mrow>  <mrow>  <mo> &#x2212; <!-- â\x88\x92 -->  </mo>  <mn> 52 </mn>  <mrow>  <mo> / </mo>  </mrow>  <mn> 45 </mn>  </mrow>  </msubsup>  <msup>  <mrow>  <mo> ( </mo>  <mfrac>  <msub>  <mi> &#x03B5; <!-- Îµ -->  </mi>  <mrow>  <mi> r </mi>  <mo> , </mo>  <mn> 01 </mn>  </mrow>  </msub>  <mi> &#x03C7; <!-- Ï\x87 -->  </mi>  </mfrac>  <mo> ) </mo>  </mrow>  <mrow>  <mn> 22 </mn>  <mrow>  <mo> / </mo>  </mrow>  <mn> 45 </mn>  </mrow>  </msup>   <mi>   </math> \n',
+    mml2: '<math> <mstyle> <mrow> <mfrac> <msub> <mi> R </mi> <mrow> <mi> s </mi> <mi> g </mi> </mrow> </msub> <msub> <mi> R </mi> <mrow> <mi> B </mi> <mi> H </mi> </mrow> </msub> </mfrac> </mrow> <mo> &#x2243; <!-- â\x89\x83 --> </mo> <mn> 5 </mn> <mo> &#x00D7; <!-- Ã\x97 --> </mo> <msup> <mn> 10 </mn> <mn> 2 </mn> </msup> <msubsup> <mi> &#x03B1; <!-- Î± --> </mi> <mrow> <mi> t </mi> <mo> , </mo> <mn> 01 </mn> </mrow> <mrow> <mn> 23 </mn> </mrow> </msubsup> <msubsup> <mi> M </mi> <mrow> <mi> B </mi> <mi> H </mi> <mo> , </mo> <mn> 8 </mn> </mrow> <mrow> <mo> &#x2212; <!-- â\x88\x92 --> </mo> <mn> <unk> </mn> </mrow> </msubsup> <msup> <mrow> <mo> ( </mo> <mstyle> <mrow> <mfrac> <msub> <mi> &#x03B5; <!-- Îµ --> </mi> <mrow> <mi> t </mi> <mo> , </mo> <mn> 01 </mn> </mrow> </msub> <mi> &#x03C7; <!-- Ï\x87 --> </mi> </mfrac> </mrow> </mstyle> <mo> ) </mo> </mrow> <mrow>\n'
+  },
+  {
+    eqn_num: 1745,
+    mml1: '<math>  <msub>  <mi> R </mi>  <mrow>  <mover>  <mi> a </mi>  <mo> &#x00AF; <!-- Â¯ -->  </mo>  </mover>  </mrow>  </msub>  <mo> = </mo>  <munder>  <mo> &#x2211; <!-- â\x88\x91 -->  </mo>  <mi> a </mi>  </munder>  <msub>  <mi> &#x03B3; <!-- Î³ -->  </mi>  <mrow>  <mover>  <mi> a </mi>  <mo> &#x00AF; <!-- Â¯ -->  </mo>  </mover>  <mi> a </mi>  </mrow>  </msub>   <mstyle>   <msubsup>  <mi> &#x0393; <!-- Î\x93 -->  </mi>  <mi> a </mi>  <mrow>  <mo> ( </mo>  <mn> 1 </mn>  <mo> ) </mo>  </mrow>  </msubsup>  <mo> = </mo>  <mi> O </mi>  <mo> ( </mo>  <mi> &#x03B6; <!-- Î¶ -->  </mi>  <mo> ) </mo>  <mo> , </mo>  </math> \n',
+    mml2: '<math> <msub> <mi> R </mi> <mi> a </mi> </msub> <mo> = </mo> <munder> <mo> &#x2211; <!-- â\x88\x91 --> </mo> <mi> a </mi> </munder> <msub> <mi> &#x03B3; <!-- Î³ --> </mi> <mrow> <mi> a </mi> <mi> a </mi> </mrow> </msub> <mstyle> <msubsup> <mi> &#x0393; <!-- Î\x93 --> </mi> <mi> a </mi> <mrow> <mo> ( </mo> <mn> 1 </mn> <mo> ) </mo> </mrow> </msubsup> <mo> = </mo> <mi> O </mi> <mo> ( </mo> <mi> &#x03B6; <!-- Î¶ --> </mi> <mo> ) </mo> <mo> , </mo> <mstyle> </math>\n'
+  },
+  {
+    eqn_num: 3308,
+    mml1: '<math>  <msub>  <mi> &#x03B2; <!-- Î² -->  </mi>  <mrow>  <mi> e </mi>  <mo> + </mo>  <mi> i </mi>  </mrow>  </msub>  <mo> ( </mo>  <msub>  <mi> &#x03BC; <!-- Î¼ -->  </mi>  <mrow>  <mn> 1 </mn>  </mrow>  </msub>  <mo> , </mo>  <mo> &#x2026; <!-- â\x80¦ -->  </mo>  <mo> , </mo>  <msub>  <mi> &#x03BC; <!-- Î¼ -->  </mi>  <mrow>  <mi> e </mi>  <mo> + </mo>  <mi> i </mi>  </mrow>  </msub>  <mo> ) </mo>  <mo> = </mo>  <msub>  <mi> &#x03B3; <!-- Î³ -->  </mi>  <mrow>  <mi> e </mi>  <mo> + </mo>  <msub>  <mi> &#x03B4; <!-- Î´ -->  </mi>  <mi> i </mi>  </msub>  </mrow>  </msub>  <mo> ( </mo>  <msub>  <mi> &#x03BC; <!-- Î¼ -->  </mi>  <mrow>  <mn> 1 </mn>  </mrow>  </msub>  <mo> , </mo>  <mo> &#x2026; <!-- â\x80¦ -->  </mo>  <mo> , </mo>  <msub>  <mi> &#x03BC; <!-- Î¼ -->  </mi>  <mrow>  <mi> p </mi>  </mrow>  </msub>  <mo> , </mo>  <msub>  <mi> &#x03BA; <!-- Îº -->  </mi>  <mn> 1 </mn>  </msub>  <mo> , </mo>  <mo> &#x2026; <!-- â\x80¦ -->  </mo>  <mo> , </mo>  <msub>  <mi> &#x03BA; <!-- Îº -->  </mi>  <mi> d </mi>  </msub>  <mo> ) </mo>   <mo>    <mo>    <mo>   <mrow>  <mi> f </mi>  <mi> o </mi>  <mi> r </mi>  </mrow>   <mo>    <mo>    <mo>   <mn> 1 </mn>  <mo> &#x2A7D; <!-- â©½ -->  </mo>  <mi> i </mi>  <mo> &#x2A7D; <!-- â©½ -->  </mo>  <msup>  <mi> d </mi>  <mrow>  <msup>  <mi>  </mi>  <mo> &#x2032; </mo>  </msup>  </mrow>  </msup>  <mo> , </mo>   <mo>   </math> \n',
+    mml2: '<math> <msub> <mi> &#x03B2; <!-- Î² --> </mi> <mrow> <mi> e </mi> <mo> + </mo> <mi> i </mi> </mrow> </msub> <mo> ( </mo> <msub> <mi> &#x03BC; <!-- Î¼ --> </mi> <mn> 1 </mn> </msub> <mo> , </mo> <mo> &#x2026; <!-- â\x80¦ --> </mo> <mo> , </mo> <msub> <mi> &#x03BC; <!-- Î¼ --> </mi> <mrow> <mi> e </mi> <mo> + </mo> <mi> i </mi> </mrow> </msub> <mo> ) </mo> <mo> = </mo> <msub> <mi> &#x03B3; <!-- Î³ --> </mi> <mrow> <mi> e </mi> <mo> + </mo> <msub> <mi> &#x03B4; <!-- Î´ --> </mi> <mi> i </mi> </msub> </mrow> </msub> <mo> ( </mo> <msub> <mi> &#x03BC; <!-- Î¼ --> </mi> <mn> 1 </mn> </msub> <mo> , </mo> <mo> &#x2026; <!-- â\x80¦ --> </mo> <mo> , </mo> <msub> <mi> &#x03BC; <!-- Î¼ --> </mi> <mi> p </mi> </msub> <mo> , </mo> <msub> <mi> &#x03F0; <!-- Ï° --> </mi> <mn> 1 </mn> </msub> <mo>\n'
+  },
+  {
+    eqn_num: 2073,
+    mml1: '<math>  <mo> ( </mo>  <msup>  <mi> x </mi>  <mrow>  <mi> &#x03BC; <!-- Î¼ -->  </mi>  </mrow>  </msup>  <msubsup>  <mo> ) </mo>  <mrow>  <mi> &#x03BC; <!-- Î¼ -->  </mi>  <mo> = </mo>  <mn> 0 </mn>  </mrow>  <mi> n </mi>  </msubsup>  </math> \n',
+    mml2: '<math> <mo> ( </mo> <msup> <mi> x </mi> <mi> &#x03BC; <!-- Î¼ --> </mi> </msup> <msubsup> <mo> ) </mo> <mrow> <mi> &#x03BC; <!-- Î¼ --> </mi> <mo> = </mo> <mn> 0 </mn> </mrow> <mi> n </mi> </msubsup> </math>\n'
+  },
+  {
+    eqn_num: 585,
+    mml1: '<math>   <mo>  &#x2016; <!-- â\x80\x96 -->  </mo>  <mi> &#x03C6; <!-- Ï\x86 -->  </mi>  <mo> ( </mo>  <mi> t </mi>  <mo> ) </mo>  <msub>   <mo>  &#x2016; <!-- â\x80\x96 -->  </mo>  <mrow>  <msup>  <mi> L </mi>  <mrow>  <mi> &#x221E; <!-- â\x88\x9e -->  </mi>  </mrow>  </msup>  <mo> ( </mo>  <mi> &#x03A9; <!-- Î© -->  </mi>  <mo> ) </mo>  </mrow>  </msub>  <mo> &#x2A7D; <!-- â©½ -->  </mo>  <msub>  <mi> &#x0393; <!-- Î\x93 -->  </mi>  <mrow>  <mi> &#x03C4; <!-- Ï\x84 -->  </mi>  </mrow>  </msub>  <mo> , </mo>   <mstyle>   </math> \n',
+    mml2: '<math> <mo> &#x2016; <!-- â\x80\x96 --> </mo> <mi> &#x03C6; <!-- Ï\x86 --> </mi> <mo> ( </mo> <mi> t </mi> <mo> ) </mo> <msub> <mo> &#x2016; <!-- â\x80\x96 --> </mo> <mrow> <msup> <mi> L </mi> <mi> &#x221E; <!-- â\x88\x9e --> </mi> </msup> <mo> ( </mo> <mi> &#x03A9; <!-- Î© --> </mi> <mo> ) </mo> </mrow> </msub> <mo> &#x2A7D; <!-- â©½ --> </mo> <msub> <mi> &#x0393; <!-- Î\x93 --> </mi> <mi> &#x03C4; <!-- Ï\x84 --> </mi> </msub> <mo> , </mo> <mstyle> </math>\n'
+  },
+  {
+    eqn_num: 14,
+    mml1: '<math>  <mi> d </mi>  <msub>  <mi> A </mi>  <mn> 1 </mn>  </msub>  <mo> ( </mo>  <mi> t </mi>  <mo> ) </mo>  <mrow>  <mo> / </mo>  </mrow>  <mi> d </mi>  <mi> t </mi>  <mo> = </mo>  <mo> &#x2212; <!-- â\x88\x92 -->  </mo>  <mo> ( </mo>  <mi> b </mi>  <mi> v </mi>  <mrow>  <mo> / </mo>  </mrow>  <mn> 2 </mn>  <mo> ) </mo>  <msub>  <mi> q </mi>  <mn> 2 </mn>  </msub>  <msub>  <mi> q </mi>  <mn> 3 </mn>  </msub>   <mi>   <msubsup>  <mi> A </mi>  <mn> 2 </mn>  <mo> &#x2217; <!-- â\x88\x97 -->  </mo>  </msubsup>  <mo> ( </mo>  <mi> t </mi>  <mo> ) </mo>  <msubsup>  <mi> A </mi>  <mn> 3 </mn>  <mrow> \n',
+    mml2: '<math> <mi> d </mi> <msub> <mi> A </mi> <mn> 1 </mn> </msub> <mo> ( </mo> <mi> t </mi> <mo> ) </mo> <mrow> <mo> / </mo> </mrow> <mi> d </mi> <mi> t </mi> <mo> = </mo> <mo> &#x2212; <!-- â\x88\x92 --> </mo> <mo> ( </mo> <mi> b </mi> <mi> v </mi> <mrow> <mo> / </mo> </mrow> <mn> 2 </mn> <mo> ) </mo> <msub> <mi> q </mi> <mn> 2 </mn> </msub> <msub> <mi> q </mi> <mn> 3 </mn> </msub> <mstyle> <msubsup> <mi> A </mi> <mn> 2 </mn> <mo> &#x2217; <!-- â\x88\x97 --> </mo> </msubsup> <mo> ( </mo> <mi> t </mi> <mo> ) </mo> <msub> <mi> A </mi> <mn> 3 </mn> </msub> <mo> ( </mo> <mi> t </mi> <mo> ) </mo> </math>\n'
+  },
+  {
+    eqn_num: 981,
+    mml1: '<math>  <mi> N </mi>  <mo> = </mo>  <mi> k </mi>  <mi> s </mi>  <mo> , </mo>   <mo>   <mi> t </mi>  <mo> = </mo>  <msup>  <mn> 2 </mn>  <mi> k </mi>  </msup>  </math> \n',
+    mml2: '<math> <mi> N </mi> <mo> = </mo> <mi> k </mi> <mi> s </mi> <mo> , </mo> <mtext> &#xA0; </mtext> <mi> t </mi> <mo> = </mo> <msup> <mn> 2 </mn> <mi> k </mi> </msup> </math>\n'
+  },
+  {
+    eqn_num: 732,
+    mml1: '<math>  <msub>  <mi> E </mi>  <mn> 0 </mn>  </msub>  <mo> = </mo>  <mo> ( </mo>  <mi> M </mi>  <mi> &#x2113; <!-- â\x84\x93 -->  </mi>  <msup>  <mo> ) </mo>  <mrow>  <mo> &#x2212; <!-- â\x88\x92 -->  </mo>   <mstyle>   <mrow>  <mfrac>  <mrow>  <mn> 2 </mn>  <mo> ( </mo>  <mi> d </mi>  <mo> &#x2212; <!-- â\x88\x92 -->  </mo>  <mn> 1 </mn>  <mo> ) </mo>  </mrow>  <mrow>  <mi> d </mi>  <mo> &#x2212; <!-- â\x88\x92 -->  </mo>  <mn> 2 </mn>  </mrow>  </mfrac>  </mrow>  </mstyle>  </mrow>  </msup>  <msup>  <mrow>  <msubsup>  <mi> c </mi>  <mn> 0 </mn>  <mi> R </mi>  </msubsup>  </mrow>  <mn> 2 </mn>  </msup>  <mstyle scriptlevel="0">  <mrow>   <mi>  ( </mo>  </mrow>  </mstyle>   <mstyle>   <mrow>  <mfrac>  <mrow>  <mi> d </mi>  <mo> &#x2212; <!-- â\x88\x92 -->  </mo>  <mn> 2 </mn>  </mrow>  <mrow>  <mn> 2 </mn>  <mi> d </mi>  </mrow>  </mfrac>  </mrow>  <msup>  <mstyle scriptlevel="0">  <mrow>   <mi>  ) </mo>  </mrow>  </mstyle>  <mrow>   <mstyle>   <mrow>  <mfrac>  <mrow>  <mi> d </mi>  <mo> + </mo>  <mn> 2 </mn>  </mrow>  <mrow>  <mi> d </mi>  <mo> &#x2212; <!-- â\x88\x92 -->  </mo>  <mn> 2 </mn>  </mrow>  </mfrac>  </mrow>  </mstyle>  </mrow>  </msup>  <msup>  <mrow>  <mo> [ </mo>  <mo> &#x2212; <!-- â\x88\x92 -->  </mo>   <mstyle>   <mrow>  <mfrac>  <mrow>  <mn> 2 </mn>  <mo> ( </mo>  <mi> d </mi>  <mo> &#x2212; <!-- â\x88\x92 -->  </mo>  <mn> 1 </mn>  <mo> ) </mo>  <mo> ( </mo>  <mi> d </mi>  <mo> &#x2212; <!-- â\x88\x92 -->  </mo>  <mn> 2 </mn>  <mo> ) </mo>  </mrow>  <msup>  <mi> d </mi>  <mn> 2 </mn>  </msup>  </mfrac>  </mrow>  <msubsup>  <mi> c </mi>  <mn> 1 </mn>  <mi> R </mi>  </msubsup>  </mstyle>  <mo> ] </mo>  </mrow>  <mrow>  <mo> &#x2212; <!-- â\x88\x92 -->  </mo>   <mstyle>   <mrow>  <mfrac>  <mi> d </mi>  <mrow>  <mi> d </mi>  <mo> &#x2212; <!-- â\x88\x92 -->  </mo>  <mn> 2 </mn>  </mrow>  </mfrac>  </mrow>  </mstyle>  </mrow>  </msup>  <mo> , </mo>  </mstyle>   <mstyle>   </math> \n',
+    mml2: '<math> <msub> <mi> E </mi> <mn> 0 </mn> </msub> <mo> = </mo> <mo> ( </mo> <mi> M </mi> <mi> &#x2113; <!-- â\x84\x93 --> </mi> <msup> <mo> ) </mo> <mrow> <mo> &#x2212; <!-- â\x88\x92 --> </mo> <mstyle> <mrow> <mfrac> <mrow> <mn> 2 </mn> <mo> ( </mo> <mi> d </mi> <mo> &#x2212; <!-- â\x88\x92 --> </mo> <mn> 1 </mn> <mo> ) </mo> </mrow> <mrow> <mi> d </mi> <mo> &#x2212; <!-- â\x88\x92 --> </mo> <mn> 2 </mn> </mrow> </mfrac> </mrow> </mstyle> </mrow> </msup> <msubsup> <mi> c </mi> <mn> 0 </mn> <mrow> <msup> <mi> R </mi> <mn> 2 </mn> </msup> </mrow> </msubsup> <mstyle scriptlevel="0"> <mrow> <mi> ( </mo> </mrow> </mstyle> <mstyle> <mrow> <mfrac> <mrow> <mi> d </mi> <mo> &#x2212; <!-- â\x88\x92 --> </mo> <mn> 2 </mn> </mrow> <mrow> <mn> 2 </mn> <mi> d </mi> </mrow> </mfrac> </mrow> <mstyle scriptlevel="0"> <mrow> <mi> [ </mo> </mrow> </mstyle> <mstyle> <mrow> <mfrac> <mrow> <mn> 2 </mn> <mo> (\n'
+  },
+  {
+    eqn_num: 3181,
+    mml1: '<math>  <msubsup>  <mrow>  <mover>  <mi> H </mi>  <mo> &#x005E; <!-- ^ -->  </mo>  </mover>  </mrow>  <mrow>  <mi> B </mi>  <mi> u </mi>  <mi> l </mi>  <mi> k </mi>  </mrow>  <mrow>  <mi> S </mi>  <mi> O </mi>  </mrow>  </msubsup>  </math> \n',
+    mml2: '<math> <msubsup> <mrow> <mover> <mi> H </mi> <mo> &#x02D9; <!-- Ë\x99 --> </mo> </mover> </mrow> <mrow> <mi> B </mi> <mi> u </mi> <mi> l </mi> <mi> k </mi> </mrow> <mrow> <mi> S </mi> <mi> O </mi> </mrow> </msubsup> </math>\n'
+  },
+  {
+    eqn_num: 296,
+    mml1: '<math>  <msup>  <mi> g </mi>  <mo> &#x2032; </mo>  </msup>  <mrow>  <mi> i </mi>  <mi> n </mi>  </mrow>  <msup>  <mi> L </mi>  <mn> 2 </mn>  </msup>  <mo> ( </mo>  <mi> I </mi>  <mo> , </mo>  <mrow>  <mrow>  <mi> T </mi>  </mrow>  </mrow>  <mi> O </mi>  <mo> ( </mo>  <mi> n </mi>  <mo> ) </mo>  <mo> ) </mo>  </math> \n',
+    mml2: '<math> <msup> <mi> g </mi> <mo> &#x2032; </mo> </msup> <mrow> <mi> i </mi> <mi> n </mi> </mrow> <msup> <mi> L </mi> <mn> 2 </mn> </msup> <mo> ( </mo> <mi> I </mi> <mo> , </mo> <mrow> <mi> T </mi> <mi> O </mi> </mrow> <mo> ( </mo> <mi> n </mi> <mo> ) </mo> <mo> ) </mo> </math>\n'
+  },
+  {
+    eqn_num: 1188,
+    mml1: '<math>  <mi> &#x03C9; <!-- Ï\x89 -->  </mi>  <mrow>  <mi> i </mi>  <mi> n </mi>  </mrow>  <msub>  <mover>  <mi> &#x03A9; <!-- Î© -->  </mi>  <mo> &#x00AF; <!-- Â¯ -->  </mo>  </mover>  <mrow>  <mi> K </mi>  <mo> &#x2212; <!-- â\x88\x92 -->  </mo>  <mn> 1 </mn>  </mrow>  </msub>  </math> \n',
+    mml2: '<math> <mi> &#x03C9; <!-- Ï\x89 --> </mi> <mrow> <mi> i </mi> <mi> n </mi> </mrow> <msub> <mi> &#x03A9; <!-- Î© --> </mi> <mrow> <mi> K </mi> <mo> &#x2212; <!-- â\x88\x92 --> </mo> <mn> 1 </mn> </mrow> </msub> </math>\n'
+  },
+  {
+    eqn_num: 3010,
+    mml1: '<math>   <mo>   <mrow>  <mi> t </mi>  <mi> F </mi>  <mi> i </mi>  <mi> x </mi>  </mrow>  <mo> ( </mo>  <mi> g </mi>  <mo> ) </mo>  <mo> = </mo>  <mi> &#x2205; <!-- â\x88\x85 -->  </mi>  <mtext> &#xA0; </mtext>  </math> \n',
+    mml2: '<math> <mrow> <mi> t </mi> <mi> F </mi> <mi> i </mi> <mi> x </mi> </mrow> <mo> ( </mo> <mi> g </mi> <mo> ) </mo> <mo> = </mo> <mi> &#x2205; <!-- â\x88 --> </mi> </math>\n'
+  },
+  {
+    eqn_num: 2184,
+    mml1: '<math>  <mi> tan </mi>  <mo> &#x2061; <!-- â\x81¡ -->  </mo>  <mi> &#x03B2; <!-- Î² -->  </mi>  <mo> = </mo>  <msub>  <mi> &#x03C5; <!-- Ï\x85 -->  </mi>  <mrow>  <msub>  <mi>  </mi>  <mi> u </mi>  </msub>  </mrow>  </msub>  <mrow>  <mo> / </mo>  </mrow>  <msqrt>  <msubsup>  <mi> &#x03C5; <!-- Ï\x85 -->  </mi>  <mrow>  <msub>  <mi>  </mi>  <mi> d </mi>  </msub>  </mrow>  <mn> 2 </mn>  </msubsup>  <mo> + </mo>  <munderover>  <mo> &#x2211; <!-- â\x88\x91 -->  </mo>  <mrow>  <mi> &#x03B1; <!-- Î± -->  </mi>  <mo> = </mo>  <mn> 1 </mn>  </mrow>  <mn> 3 </mn>  </munderover>  <msubsup>  <mi> &#x03C5; <!-- Ï\x85 -->  </mi>  <mrow>  <msub>  <mi>  </mi>  <mrow>  <msub>  <mi> L </mi>  <mi> &#x03B1; <!-- Î± -->  </mi>  </msub>  </mrow>  </msub>  </mrow>  <mn> 2 </mn>  </msubsup>  </msqrt>   <mi>   </math> \n',
+    mml2: '<math> <mi> tan </mi> <mo> &#x2061; <!-- â\x81¡ --> </mo> <mi> &#x03B2; <!-- Î² --> </mi> <mo> = </mo> <msub> <mi> &#x03C5; <!-- Ï --> </mi> <mrow> <msub> <mi> </mi> <mi> u </mi> </msub> </mrow> </msub> <mrow> <mo> / </mo> </mrow> <msqrt> <msubsup> <mi> &#x03C5; <!-- Ï --> </mi> <mi> d </mi> <mn> 2 </mn> </msubsup> <mo> + </mo> <munderover> <mo> &#x2211; <!-- â\x88\x91 --> </mo> <mrow> <mi> &#x03B1; <!-- Î± --> </mi> <mo> = </mo> <mn> 1 </mn> </mrow> <mn> 3 </mn> </munderover> <msubsup> <mi> v </mi> <mrow> <msub> <mi> L </mi> <mi> &#x03B1; <!-- Î± --> </mi> </msub> </mrow> <mn> 2 </mn> </msubsup> </msqrt> </math>\n'
+  },
+];
