1d2f196c6c68487c4d0bc4141a9de0227e6488c8
gauravsh0812 <gauravs@email.arizona.edu>
2021-07-01 22:31:11 -0400

    add

--- temp.py
+++ temp.py
@@ -12,20 +12,20 @@ def main():
     year = '20'+str(y)
     year_path = os.path.join(root, year)
     for m in range(1, 13):
-    mm = f"{m:02d}"
-    month = str(y)+mm
-    month_path = os.path.join(year_path, month)
-    etree = os.path.join(month_path, 'etree')
-    for folder_path in glob.glob(os.path.join(etree, '*')):
-      folder = os.path.basename(folder_path)
-      for tyf in glob.glob(os.path.join(folder_path, '*')):
-        tyf_basename = os.path.basename(tyf)
-        TYF = 'Large_eqns' if tyf_basename == 'Large_MML' else 'Small_eqns'
-        for file_name_path in glob.glob(os.path.join(tyf, '*')):
-          file_name = os.path.basename(file_name_path)
-          file_name = file_name.split('.')[0]
+      mm = f"{m:02d}"
+      month = str(y)+mm
+      month_path = os.path.join(year_path, month)
+      etree = os.path.join(month_path, 'etree')
+      for folder_path in glob.glob(os.path.join(etree, '*')):
+        folder = os.path.basename(folder_path)
+        for tyf in glob.glob(os.path.join(folder_path, '*')):
+          tyf_basename = os.path.basename(tyf)
+          TYF = 'Large_eqns' if tyf_basename == 'Large_MML' else 'Small_eqns'
+          for file_name_path in glob.glob(os.path.join(tyf, '*')):
+            file_name = os.path.basename(file_name_path)
+            file_name = file_name.split('.')[0]
 
-          temp.append([root, year, month, folder, TYF, file_name])
+            temp.append([root, year, month, folder, TYF, file_name])
 
   with Pool(multiprocessing.cpu_count()-10) as pool:
     result = pool.map(main_parallel, temp)
