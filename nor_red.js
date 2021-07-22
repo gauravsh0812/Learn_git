c510b03f23a8c2d8af1d396b6659ac4d30ac5b66
Gaurav Sharma <gauravs@sista.arizona.edu>
2021-07-22 08:08:28 -0700

    add

---
+++ nor_red.js
@@ -0,0 +1,52 @@
+eqn_src = [
+  {
+    eqn_num: 0,
+    mml1: '<math>  <msub>    <mi>p</mi>    <mrow>      <mo>!</mo>    </mrow>  </msub>  <mo>(</mo>  <mi>X</mi>  <mrow>    <mo>&#x27F6;<!-- â\x9f¶ --></mo>  </mrow>  <mi>B</mi>  <mo>)</mo>  <mo>=</mo>  <mi>X</mi></math>\n',
+    mml2: '<math> <msub> <mi>p</mi> <mo>!</mo> </msub> <mo>(</mo> <mi>X</mi> <mrow> <mo>&#x27F6;<!-- â\x9f¶ --></mo> </mrow> <mi>B</mi> <mo>)</mo> <mo>=</mo> <mi>X</mi></math>\n'
+  },
+  {
+    eqn_num: 1,
+    mml1: '<math>  <munder>    <mo>&#x2211;<!-- â\x88\x91 --></mo>    <mrow>      <mi>v</mi>      <mo>&#x2208;<!-- â\x88\x88 --></mo>      <msup>        <mrow>           <mi> Q</mi>        </mrow>        <mi>n</mi>      </msup>    </mrow>  </munder>  <mrow>    <mo>|</mo>    <mrow>      <mi>f</mi>      <mo>(</mo>      <mi>&#x03B1;<!-- Î± --></mi>      <mo>(</mo>      <mi>v</mi>      <mo>)</mo>      <mo>)</mo>    </mrow>    <mo>|</mo>  </mrow>  <msub>    <mi>&#x03BB;<!-- Î» --></mi>    <mrow>      <mi>c</mi>      <mo>,</mo>      <mi>n</mi>    </mrow>  </msub>  <msup>    <mi>L</mi>    <mi>n</mi>  </msup>  <munder>    <mo>&#x2211;<!-- â\x88\x91 --></mo>    <mrow>      <mi>I</mi>      <mo>&#x2286;<!-- â\x8a\x86 --></mo>       <mo> {</mo>      <mn>1</mn>      <mo>,</mo>      <mo>&#x2026;<!-- â\x80¦ --></mo>      <mo>,</mo>      <mi>n</mi>       <mo> }</mo>    </mrow>  </munder></math>\n',
+    mml2: '<math> <munder> <mo>&#x2211;<!-- â\x88\x91 --></mo> <mrow> <mi>v</mi> <mo>&#x2208;<!-- â\x88\x88 --></mo> <msup> <mrow> <mi> Q</mi> </mrow> <mi>n</mi> </msup> </mrow> </munder> <mrow> <mo>|</mo> <mrow> <mi>f</mi> <mo>(</mo> <mi>&#x03B1;<!-- Î± --></mi> <mo>(</mo> <mi>v</mi> <mo>)</mo> <mo>)</mo> </mrow> <mo>|</mo> </mrow> <msub> <mi>&#x03BB;<!-- Î» --></mi> <mrow> <mi>c</mi> <mo>,</mo> <mi>n</mi> </mrow> </msub> <msup> <mi>L</mi> <mi>n</mi> </msup> <munder> <mo>&#x2211;<!-- â\x88\x91 --></mo> <mrow> <mi>I</mi> <mo>&#x2286;<!-- â\x8a\x86 --></mo> <mo> {</mo> <mn>1</mn> <mo>,</mo> <mo>&#x2026;<!-- â\x80¦ --></mo> <mo>,</mo> <mi>n</mi> <mo> }</mo> </mrow> </munder></math>\n'
+  },
+  {
+    eqn_num: 2,
+    mml1: '<math>  <mi>&#x03BB;<!-- Î» --></mi>  <mo>=</mo>  <mn>0</mn>  <mo>,</mo>  <mo>&#x00B1;<!-- Â± --></mo></math>\n',
+    mml2: '<math> <mi>&#x03BB;<!-- Î» --></mi> <mo>=</mo> <mn>0</mn> <mo>,</mo> <mo>&#x00B1;<!-- Â± --></mo></math>\n'
+  },
+  {
+    eqn_num: 3,
+    mml1: '<math>  <mo>(</mo>  <mi>&#x03BB;<!-- Î» --></mi>  <mi>v</mi>  <msup>    <mi>u</mi>    <mo>&#x2032;</mo>  </msup>  <mo>+</mo>  <mo>(</mo>  <mi>I</mi>  <mo>&#x2212;<!-- â\x88\x92 --></mo>  <mi>v</mi>  <msup>    <mi>u</mi>    <mo>&#x2032;</mo>  </msup>  <mo>)</mo>  <mi>A</mi>  <msup>    <mo>)</mo>    <mi>k</mi>  </msup></math>\n',
+    mml2: '<math> <mo>(</mo> <mi>&#x03BB;<!-- Î» --></mi> <mi>v</mi> <msup> <mi>u</mi> <mo>&#x2032;</mo> </msup> <mo>+</mo> <mo>(</mo> <mi>I</mi> <mo>&#x2212;<!-- â\x88\x92 --></mo> <mi>v</mi> <msup> <mi>u</mi> <mo>&#x2032;</mo> </msup> <mo>)</mo> <mi>A</mi> <msup> <mo>)</mo> <mi>k</mi> </msup></math>\n'
+  },
+  {
+    eqn_num: 4,
+    mml1: '<math>  <msub>    <mi>M</mi>    <mi>R</mi>  </msub>  <mo>=</mo>  <mo>&#x2212;<!-- â\x88\x92 --></mo>  <mn>14.51</mn></math>\n',
+    mml2: '<math> <msub> <mi>M</mi> <mi>R</mi> </msub> <mo>=</mo> <mo>&#x2212;<!-- â\x88\x92 --></mo> <mn>14.51</mn></math>\n'
+  },
+  {
+    eqn_num: 5,
+    mml1: '<math>  <msub>    <mi>u</mi>    <mn>1</mn>  </msub>  <mo>=</mo>  <msup>    <mi>e</mi>    <mrow>      <mi>i</mi>      <mi>t</mi>      <mi>L</mi>      <mo>(</mo>      <mi>&#x03BB;<!-- Î» --></mi>      <mo>)</mo>    </mrow>  </msup>  <mi>f</mi></math>\n',
+    mml2: '<math> <msub> <mi>u</mi> <mn>1</mn> </msub> <mo>=</mo> <msup> <mi>e</mi> <mrow> <mi>i</mi> <mi>t</mi> <mi>L</mi> <mo>(</mo> <mi>&#x03BB;<!-- Î» --></mi> <mo>)</mo> </mrow> </msup> <mi>f</mi></math>\n'
+  },
+  {
+    eqn_num: 6,
+    mml1: '<math>  <mi>k</mi>  <mo>=</mo>  <mn>2</mn></math>\n',
+    mml2: '<math> <mi>k</mi> <mo>=</mo> <mn>2</mn></math>\n'
+  },
+  {
+    eqn_num: 7,
+    mml1: '<math>  <mi>a</mi>  <mo>=</mo>  <msup>    <mi>&#x03A6;<!-- Î¦ --></mi>    <mrow>      <mo>&#x2212;<!-- â\x88\x92 --></mo>      <mn>1</mn>    </mrow>  </msup>  <mo>(</mo>  <mn>1</mn>  <mo>&#x2212;<!-- â\x88\x92 --></mo>  <msup>    <mn>2</mn>    <mrow>      <mo>&#x2212;<!-- â\x88\x92 --></mo>      <mo>(</mo>      <mi>p</mi>      <mo>+</mo>      <mn>1</mn>      <mo>)</mo>    </mrow>  </msup>  <mo>)</mo></math>\n',
+    mml2: '<math> <mi>a</mi> <mo>=</mo> <msup> <mi>&#x03A6;<!-- Î¦ --></mi> <mrow> <mo>&#x2212;<!-- â\x88\x92 --></mo> <mn>1</mn> </mrow> </msup> <mo>(</mo> <mn>1</mn> <mo>&#x2212;<!-- â\x88\x92 --></mo> <msup> <mn>2</mn> <mrow> <mo>&#x2212;<!-- â\x88\x92 --></mo> <mo>(</mo> <mi>p</mi> <mo>+</mo> <mn>1</mn> <mo>)</mo> </mrow> </msup> <mo>)</mo></math>\n'
+  },
+  {
+    eqn_num: 8,
+    mml1: '<math>  <munderover>    <mo>&#x2211;<!-- â\x88\x91 --></mo>    <mrow>      <mi>i</mi>      <mo>=</mo>      <mn>1</mn>    </mrow>    <mrow>      <mi>n</mi>      <mo>&#x2212;<!-- â\x88\x92 --></mo>      <mn>1</mn>    </mrow>  </munderover>  <mo>(</mo>  <mo>&#x2212;<!-- â\x88\x92 --></mo>  <mn>1</mn>  <msup>    <mo>)</mo>    <mrow>      <mi>i</mi>    </mrow>  </msup>  <msub>    <mi>M</mi>    <mi>i</mi>  </msub>  <mo>&#x2218;<!-- â\x88\x98 --></mo>  <msub>    <mi>M</mi>    <mrow>      <mi>n</mi>      <mo>&#x2212;<!-- â\x88\x92 --></mo>      <mi>i</mi>      <mo>+</mo>      <mn>1</mn>    </mrow>  </msub>  <mo>=</mo>  <mn>0</mn></math>\n',
+    mml2: '<math> <munderover> <mo>&#x2211;<!-- â\x88\x91 --></mo> <mrow> <mi>i</mi> <mo>=</mo> <mn>1</mn> </mrow> <mrow> <mi>n</mi> <mo>&#x2212;<!-- â\x88\x92 --></mo> <mn>1</mn> </mrow> </munderover> <mo>(</mo> <mo>&#x2212;<!-- â\x88\x92 --></mo> <mn>1</mn> <msup> <mo>)</mo> <mi>i</mi> </msup> <msub> <mi>M</mi> <mi>i</mi> </msub> <mo>&#x2218;<!-- â\x88\x98 --></mo> <msub> <mi>M</mi> <mrow> <mi>n</mi> <mo>&#x2212;<!-- â\x88\x92 --></mo> <mi>i</mi> <mo>+</mo> <mn>1</mn> </mrow> </msub> <mo>=</mo> <mn>0</mn></math>\n'
+  },
+  {
+    eqn_num: 9,
+    mml1: '<math>  <mi>&#x03C9;<!-- Ï\x89 --></mi>  <mo>&#x2208;<!-- â\x88\x88 --></mo>  <msup>    <mi>&#x03A9;<!-- Î© --></mi>    <mi>p</mi>  </msup>  <mo>(</mo>  <mi>&#x2202;<!-- â\x88\x82 --></mi>  <mi>M</mi>  <mo>)</mo></math>\n',
+    mml2: '<math> <mi>&#x03C9;<!-- Ï\x89 --></mi> <mo>&#x2208;<!-- â\x88\x88 --></mo> <msup> <mi>&#x03A9;<!-- Î© --></mi> <mi>p</mi> </msup> <mo>(</mo> <mi>&#x2202;<!-- â\x88\x82 --></mi> <mi>M</mi> <mo>)</mo></math>\n'
+  },
+];
