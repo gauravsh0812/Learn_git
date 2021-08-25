d770168b3b68820888a8690fea9a7716fa4a58f2
gauravsh0812 <gauravs@email.arizona.edu>
2021-08-25 16:59:04 -0700

    add

---
+++ temp.py
@@ -0,0 +1,53 @@
+import os, PIL, multiprocessing
+from PIL import Image
+from multiprocessing import Lock, Pool
+
+lock=Lock()
+
+def main(src):
+
+  global lock
+
+  temp = []
+  for idx, s in enumerate(src[:100]):
+    month, f, t, name = s.split('_')
+    year = '20'+str(month[0:2])
+    folder = str(month)+'.'+str(f)
+    tyf = str(t)+'_eqns'
+    eqn_name = name.split('.')[0]+'.txt'
+    s_temp=s.replace('\n','')
+    img_path = f'/projects/temporary/automates/er/gaurav/data_17M/xfer/images/{s_temp}'
+
+    temp.append([img_path, year, month, folder, tyf, eqn_name])
+
+  print('temp done!')
+
+  pool = Pool(multiprocessing.cpu_count()-10)
+  with open('/projects/temporary/automates/er/gaurav/data_17M/xfer/tgt-train-MP.txt', 'w') as tgt:
+    with open('/projects/temporary/automates/er/gaurav/data_17M/xfer/src-train-new-MP.txt', 'w') as src_new:
+      for result in pool.imap(parallel, temp):
+        if Flag:
+          lock.acquire()
+          tgt.write(eqn)
+          src.write(img_path)
+          lock.release()
+
+
+def parallel(args):
+
+  img_path, year, month, folder, tyf, eqn_name = args
+  Flag=False
+  try:
+    IMG = Image.open(img_path)
+    final_path = f'/projects/temporary/automates/er/gaurav/{year}/{month}/latex_equations/{folder}/{tyf}/{eqn_name}'
+    eqn = open(final_path, 'r').readlines()[0]
+    Flag = True
+    return eqn, img_path, Flag
+
+  except:
+    return Flag
+    pass
+
+if __name__ == '__main__':
+    src = open('/projects/temporary/automates/er/gaurav/data_17M/xfer/src-train.txt', 'r').readlines()
+    main(src)
