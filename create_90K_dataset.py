051098967b1d1cefaa974a6576da4cfe3072c4f9
gauravsh0812 <gauravs@arizona.edu>
2022-10-28 12:06:30 -0700

    add

--- create_90K_dataset.py
+++ create_90K_dataset.py
@@ -37,63 +37,66 @@ def write_eqn(mml, latex, imagepath, NEqn):
         shutil.copyfile(imagepath, dst)
 
 NEqn = 0
-yr = str(random.sample(years, 1)[0])
-month = random.sample(months, 1)[0]
-p1 = os.path.join(root, f"20{yr}/{yr}{month}" )
-etrees = os.path.join(p1, "etree")
-etree_list = random.sample(os.listdir(etrees),10)
+keep_going = True
+while keep_going:
+    yr = str(random.sample(years, 1)[0])
+    month = random.sample(months, 1)[0]
+    p1 = os.path.join(root, f"20{yr}/{yr}{month}" )
+    etrees = os.path.join(p1, "etree")
+    etree_list = random.sample(os.listdir(etrees),10)
 
-for folder in etree_list:
-    if NEqn<=10000:
-        print(NEqn)
-        p2 = os.path.join(etrees, folder)
-        for tyf in os.listdir(p2):
-            p3 = os.path.join(p2, tyf)
-            for eqn in os.listdir(p3):
-                eqn_num = eqn.split(".")[0]
-                mmlpath = root + f"/20{yr}/{yr}{month}/Simplified_mml/{folder}/{tyf}/{eqn_num}.txt"
-                latexpath = root + f"/20{yr}/{yr}{month}/latex_equations/{folder}/{tyf.split('_')[0]}_eqns/{eqn_num}.txt"
+    for folder in etree_list:
+        if NEqn<=10000:
+            print(NEqn)
+            p2 = os.path.join(etrees, folder)
+            for tyf in os.listdir(p2):
+                p3 = os.path.join(p2, tyf)
+                for eqn in os.listdir(p3):
+                    eqn_num = eqn.split(".")[0]
+                    mmlpath = root + f"/20{yr}/{yr}{month}/Simplified_mml/{folder}/{tyf}/{eqn_num}.txt"
+                    latexpath = root + f"/20{yr}/{yr}{month}/latex_equations/{folder}/{tyf.split('_')[0]}_eqns/{eqn_num}.txt"
 
-                imagepath = root + f"/20{yr}/{yr}{month}/latex_images/{folder}/{tyf.split('_')[0]}_eqns/{eqn_num}.png0001-1.png"
-                if not os.path.exists(imagepath):
-                    imagepath = root + f"/20{yr}/{yr}{month}/latex_images/{folder}/{tyf.split('_')[0]}_eqns/{eqn_num}.png"
+                    imagepath = root + f"/20{yr}/{yr}{month}/latex_images/{folder}/{tyf.split('_')[0]}_eqns/{eqn_num}.png0001-1.png"
+                    if not os.path.exists(imagepath):
+                        imagepath = root + f"/20{yr}/{yr}{month}/latex_images/{folder}/{tyf.split('_')[0]}_eqns/{eqn_num}.png"
 
-                latex = open(latexpath).readlines()[0]
-                mml = open(mmlpath).readlines()[0]
+                    latex = open(latexpath).readlines()[0]
+                    mml = open(mmlpath).readlines()[0]
 
-                if (len(latex)>10) and (len(mml.split())>5) and ("&#xA0" not in mml):
+                    if (len(latex)>10) and (len(mml.split())>5) and ("&#xA0" not in mml):
 
-                    tok_mml, tok_len = token_main(mml)
-                    if (tok_len < 50):# and (count_50 <= 10000):
-                        count_50+=1
-                        write_eqn(mml, latex, imagepath, NEqn)
-                        NEqn+=1
-                    # elif (tok_len>=50 and tok_len < 100) and (count_100 <= 35000):
-                    #     count_100+=1
-                    #     write_eqn(mml, latex, imagepath, NEqn)
-                    #     NEqn+=1
-                    # elif (tok_len>=100 and tok_len < 150) and (count_150 <= 25000):
-                    #     count_150+=1
-                    #     write_eqn(mml, latex, imagepath, NEqn)
-                    #     NEqn+=1
-                    # elif (tok_len>=150 and tok_len < 200) and (count_200 <= 12000):
-                    #     count_200+=1
-                    #     write_eqn(mml, latex, imagepath, NEqn)
-                    #     NEqn+=1
-                    # elif (tok_len>=200 and tok_len < 250) and (count_250 <= 5000):
-                    #     count_250+=1
-                    #     write_eqn(mml, latex, imagepath, NEqn)
-                    #     NEqn+=1
-                    # elif (tok_len>=250 and tok_len < 300) and (count_300 <= 2000):
-                    #     count_300+=1
-                    #     write_eqn(mml, latex, imagepath, NEqn)
-                    #     NEqn+=1
-                    # elif (tok_len>=300) and (count_gt300 <= 1000):
-                    #     count_gt300+=1
-                    #     write_eqn(mml, latex, imagepath, NEqn)
-                    #     NEqn+=1
+                        tok_mml, tok_len = token_main(mml)
+                        if (tok_len < 50):# and (count_50 <= 10000):
+                            count_50+=1
+                            write_eqn(mml, latex, imagepath, NEqn)
+                            NEqn+=1
+                        # elif (tok_len>=50 and tok_len < 100) and (count_100 <= 35000):
+                        #     count_100+=1
+                        #     write_eqn(mml, latex, imagepath, NEqn)
+                        #     NEqn+=1
+                        # elif (tok_len>=100 and tok_len < 150) and (count_150 <= 25000):
+                        #     count_150+=1
+                        #     write_eqn(mml, latex, imagepath, NEqn)
+                        #     NEqn+=1
+                        # elif (tok_len>=150 and tok_len < 200) and (count_200 <= 12000):
+                        #     count_200+=1
+                        #     write_eqn(mml, latex, imagepath, NEqn)
+                        #     NEqn+=1
+                        # elif (tok_len>=200 and tok_len < 250) and (count_250 <= 5000):
+                        #     count_250+=1
+                        #     write_eqn(mml, latex, imagepath, NEqn)
+                        #     NEqn+=1
+                        # elif (tok_len>=250 and tok_len < 300) and (count_300 <= 2000):
+                        #     count_300+=1
+                        #     write_eqn(mml, latex, imagepath, NEqn)
+                        #     NEqn+=1
+                        # elif (tok_len>=300) and (count_gt300 <= 1000):
+                        #     count_gt300+=1
+                        #     write_eqn(mml, latex, imagepath, NEqn)
+                        #     NEqn+=1
 
-    else:
-        print("breaking at...")
-        print(NEqn)
-        break
+        else:
+            keep_going=False
+            print("breaking at...")
+            print(NEqn)
+            break
